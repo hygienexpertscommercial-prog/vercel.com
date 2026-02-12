a12ad05cede0fd698d7c2becfb78012ed78d4c41
hygienexpertscommercial-prog <hygienexpertscommercial@gmail.com>
2026-02-12 15:58:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('HygienExperts Website Ready');
+const faders=document.querySelectorAll('.fade-in');
+const appearOptions={threshold:0.3};
+const appearOnScroll=new IntersectionObserver(function(entries,observer){
+entries.forEach(entry=>{
+if(!entry.isIntersecting)return;
+entry.target.classList.add('active');
+observer.unobserve(entry.target);
+});
+},appearOptions);
+faders.forEach(fader=>{appearOnScroll.observe(fader);});
